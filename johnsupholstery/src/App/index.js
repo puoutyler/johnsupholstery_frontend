bd8cb818b52759cedde11186366ed567bb7dfa5d
Tyler Puou <tylerpuou@Shalenes-MBP.fios-router.home>
2020-06-27 18:32:46 -0700

    react app created

---
+++ johnsupholstery/src/App/index.js
@@ -0,0 +1,11 @@
+/////////////////////////-*
+//    IMPORTS
+/////////////////////////-*
+import React from "react";
+import "./style.scss";
+
+const App = (props) => {
+    return <div className="App">Hello World</div>;
+}
+
+export default App;
